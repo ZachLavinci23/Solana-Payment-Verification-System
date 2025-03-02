ff7a7838c9b89dce9817fa76881792175620bea7
Zach LaVinci <mosaiq.minds@gmail.com>
2025-03-02 21:46:56 +0100

    API REQUESTS

--- express.js
+++ express.js
@@ -1,1 +1,4 @@
+This Script need to make the API requests to SOLSCAN. 
+Its just a node thats be used to make the requests faster, otherwise every user of your bot would use the same route to make the requests, with express you outsource the API request, but you still need to build the logic for it. 
 
+Do What The Fuck You Want To Do With This Info :*
