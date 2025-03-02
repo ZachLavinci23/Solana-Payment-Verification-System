9fcb7cb7a0b5fe474c76486eed5989f41ee6c5da
Zach LaVinci <mosaiq.minds@gmail.com>
2025-03-02 21:53:38 +0100

    Update express.js

--- express.js
+++ express.js
@@ -1,1 +1,5 @@
+Express is just a minimalstic Framework to do a node thats making API requests.
 
+U still need to build a logic thats doing the API request on SOLSCAN. Express should be a nice way to do it, cause when every user is doing the request through your main script of the bot it could get slow. 
+
+DO What The Fuck You Want To Do With This Info :*
